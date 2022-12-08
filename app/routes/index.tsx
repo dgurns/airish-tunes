152d8f5cbdfff77797530492c395ef80c49f6a15
Dan Gurney <dan@dangurney.net>
2022-12-08 12:24:42 +0000

    feat: remove profiling

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,23 +4,19 @@ import { createDBClient } from '~/db.server';
 import type { LoaderArgs } from '~/types';
 
 export async function loader({ context }: LoaderArgs) {
-	const start = Date.now();
 	const db = createDBClient(context.DB);
 	const tuneImages = await db.selectFrom('tune_images').selectAll().execute();
-	const elapsed = Date.now() - start;
-	return json({ tuneImages, elapsed });
+	return json({ tuneImages });
 }
 
 export default function Index() {
-	const { tuneImages, elapsed } = useLoaderData<typeof loader>();
-
+	const { tuneImages } = useLoaderData<typeof loader>();
 	return (
 		<div className="flex flex-col space-y-8">
 			<h1>AI-rish Tunes</h1>
 			{tuneImages.map((t) => (
 				<p key={t.id}>{JSON.stringify(t)}</p>
 			))}
-			Query latency: {elapsed}ms
 		</div>
 	);
 }
