159f3005799ef0fd6a2c5bd3a79d501c6349c84b
Dan Gurney <dan@dangurney.net>
2023-01-22 18:28:54 +0000

    refactor: don't cache tune-images index page

--- app/routes/tune-images._index.tsx
+++ app/routes/tune-images._index.tsx
@@ -1,6 +1,5 @@
-import { defer } from '@remix-run/cloudflare';
-import { useLoaderData, Await } from '@remix-run/react';
-import { Suspense } from 'react';
+import { json } from '@remix-run/cloudflare';
+import { useLoaderData } from '@remix-run/react';
 import { createDBClient } from '~/db.server';
 import type { LoaderArgs } from '~/types';
 
@@ -14,41 +13,27 @@ export function headers() {
 export async function loader({ context }: LoaderArgs) {
 	// get all tune images
 	const db = createDBClient(context.DB);
-	const tuneImagesPromise = db
+	const tuneImages = await db
 		.selectFrom('tune_images')
 		.selectAll()
 		.groupBy('tune_name')
 		.orderBy('days_into_year', 'desc')
 		.execute();
-	return defer({ tuneImagesPromise }, { status: 200 });
+	return json({ tuneImages }, { status: 200 });
 }
 
 export default function TuneImagesIndex() {
-	const { tuneImagesPromise } = useLoaderData<typeof loader>();
+	const { tuneImages } = useLoaderData<typeof loader>();
 
-	return (
-		<Suspense
-			fallback={
-				<p className="text-gray-400 flex flex-col items-start w-full">
-					Loading...
-				</p>
-			}
-		>
-			<Await resolve={tuneImagesPromise}>
-				{(tuneImages) =>
-					tuneImages.length === 0 ? (
-						<p className="text-gray-400">No images generated yet</p>
-					) : (
-						<ul className="flex flex-col items-start list-none w-full space-y-1">
-							{tuneImages.map((t) => (
-								<li key={t.id}>
-									<a href={`/tune-images/${t.id}`}>{t.tune_name}</a>
-								</li>
-							))}
-						</ul>
-					)
-				}
-			</Await>
-		</Suspense>
+	return tuneImages.length === 0 ? (
+		<p className="text-gray-400">No images generated yet</p>
+	) : (
+		<ul className="flex flex-col items-start list-none w-full space-y-1">
+			{tuneImages.map((t) => (
+				<li key={t.id}>
+					<a href={`/tune-images/${t.id}`}>{t.tune_name}</a>
+				</li>
+			))}
+		</ul>
 	);
 }
